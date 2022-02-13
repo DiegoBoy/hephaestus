e755f2a65693cb9a20c7299c5ba159b346a8a10a
0xdb <>
2022-02-12 19:58:20 -0800

    Web and DNS (vhost) enum wordlists

--- install.sh
+++ install.sh
@@ -3,15 +3,16 @@
 echo "Installing..."
 sudo apt-get update > /dev/null
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 
 ### BinExp tools
 echo "[X] BinExp"
-mkdir -p $SCRIPT_DIR/BinExp
+mkdir -p BinExp
 
 # gdb-gef
 sudo apt-get install gdb -y
-mkdir -p $SCRIPT_DIR/BinExp/gdb-gef
-wget -q -O $SCRIPT_DIR/BinExp/gdb-gef/.gdbinit-gef.py https://github.com/hugsy/gef/raw/master/gef.py
+mkdir -p BinExp/gdb-gef
+wget -q -O BinExp/gdb-gef/.gdbinit-gef.py https://github.com/hugsy/gef/raw/master/gef.py
 echo "source $SCRIPT_DIR/BinExp/gdb-gef/.gdbinit-gef.py" >> ~/.gdbinit
 
 # pwntools
@@ -45,16 +46,16 @@ sudo apt-get install netcat-openbsd -y
 
 ### PrivEsc tools
 echo "[X] PrivEsc"
-mkdir -p $SCRIPT_DIR/PrivEsc
+mkdir -p PrivEsc
 
 # peas
-cd $SCRIPT_DIR/PrivEsc
+cd PrivEsc
 wget -q -o /dev/null https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh
 wget -q -o /dev/null https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe --output-document=winpeas.exe
 popd 2>/dev/null
 
 #pspy
-cd $SCRIPT_DIR/PrivEsc
+cd PrivEsc
 wget -q -o /dev/null https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32
 wget -q -o /dev/null https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64
 popd 2>/dev/null
@@ -63,23 +64,23 @@ popd 2>/dev/null
 
 ### Post tools
 echo "[X] Post"
-mkdir -p $SCRIPT_DIR/Post
+mkdir -p Post
 
 # nishang
-git clone https://github.com/samratashok/nishang.git $SCRIPT_DIR/Post/nishang
+git clone https://github.com/samratashok/nishang.git Post/nishang
 
 # PowerSploit 
-git clone https://github.com/PowerShellMafia/PowerSploit.git $SCRIPT_DIR/Post/PowerSploit
+git clone https://github.com/PowerShellMafia/PowerSploit.git Post/PowerSploit
 
 # Empire
-git clone https://github.com/EmpireProject/Empire.git $SCRIPT_DIR/Post/Empire
+git clone https://github.com/EmpireProject/Empire.git Post/Empire
 
 
 
 ### Report tools
 echo "[X] Report"
 
-cd $SCRIPT_DIR/Report/AutoRePort/
+cd Report/AutoRePort/
 sudo apt install -y npm node-typescript
 sudo npm link
 popd
@@ -87,4 +88,5 @@ popd
 
 
 # go hack stuff
+popd
 echo "hephaestus toolkit installed, go hack stuff x_x"
